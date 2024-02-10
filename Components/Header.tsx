c30be433c87c7db29726686ab1fc629bb5a6d266
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-10 11:53:40 +0530

    designing the header components almost ready

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,12 +1,20 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Search } from "lucide-react";
+import {
+  Grid2X2,
+  Heart,
+  LayoutGrid,
+  Search,
+  ShoppingCart,
+  User,
+} from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="flex bg-walmart px-10 py-7 space-x-5">
-      <Link href="/" className="">
+    <header className="flex flex-col md:flex-row items-center bg-walmart px-10 py-7 space-x-5">
+      <Link href="/" className="mb-5 md:mb-0">
         <Image
           src="https://links.papareact.com/yur"
           alt="Logo"
@@ -24,6 +32,52 @@ const Header = () => {
           <Search className="rounded-full h-10 px-3 w-10 bg-yellow-400 cursor-pointer" />
         </button>
       </form>
+      <div className="flex space-x-5 mt-5 md:mt-0">
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Grid2X2 size={20} />
+          <p>Departments</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <LayoutGrid size={20} />
+          <p>Services</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Heart size={20} />
+          <div>
+            <p className="text-xs font-extralight">Recorder</p>
+            <p>My Items</p>
+          </div>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <User size={20} />
+          <div>
+            <p className="text-xs font-extralight">Sign In</p>
+            <p>Account</p>
+          </div>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <ShoppingCart size={20} />
+          <div>
+            <p className="text-xs font-extralight">No Items</p>
+            <p>$0.00</p>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 };
