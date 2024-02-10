73973f09de2c989852169fe93dbf59fd034033c8
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-10 19:28:54 +0530

    skeleton on searching implientation

---
+++ app/search/loading.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "@/Components/ui/skeleton";
+
+const Loading = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2">Scrapping Results</h1>
+      <h2 className="mb-5 text-gray-400">We won&apos;t be long</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
