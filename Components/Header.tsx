2dd64813757a4c838f30803e24685d86b52c5c15
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-10 11:28:41 +0530

    Form of serching in header styling

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Button } from "./ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { Search } from "lucide-react";
 
 const Header = () => {
   return (
-    <header>
-      <Link href="/">
+    <header className="flex bg-walmart px-10 py-7 space-x-5">
+      <Link href="/" className="">
         <Image
-          src="https://links.papareact.com/xsi"
+          src="https://links.papareact.com/yur"
           alt="Logo"
           width={150}
           height={150}
         />
       </Link>
+      <form className="flex items-center bg-white rounded-full w-full flex-1">
+        <input
+          type="text"
+          placeholder="Search Everything..."
+          className="flex-1 px-5  rounded-l-full outline-none placeholder:text-sm"
+        />
+        <button type="submit">
+          <Search className="rounded-full h-10 px-3 w-10 bg-yellow-400 cursor-pointer" />
+        </button>
+      </form>
     </header>
   );
 };
