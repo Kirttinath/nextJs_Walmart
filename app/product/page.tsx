681439d2621aede6c52e15fc153876ea9ef9b821
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-11 23:17:28 +0530

    fetch product content implimentation complete

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,10 +1,14 @@
+import fetchProduct from "@/lib/fetchProduct";
+
 type Props = {
   searchParams: {
     url: string;
   };
 };
 const productPage = async ({ searchParams: { url } }: Props) => {
-    const product = await fetchProduct(url);
+  const product = await fetchProduct(url);
+  console.log(product);
+
   return <div>Product</div>;
 };
 
