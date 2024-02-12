b6d2433fc473856cc40d7e34d1aad71b2c2258e8
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-12 20:08:56 +0530

    product details added to the ui page of product details page

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -8,6 +8,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/Components/ui/carousel";
+import { Badge } from "@/Components/ui/badge";
 
 type Props = {
   searchParams: {
@@ -34,13 +35,13 @@ const productPage = async ({ searchParams: { url } }: Props) => {
       </div>
       <Carousel
         opts={{ loop: true }}
-        className="w-3/5 mb-10 lg:mb-0 lg:w-full self-start flex items-center max-w-xl mx-auto lg:mx:20"
+        className="w-3/5 mb-10 lg:mb-0 lg:w-full self-start flex items-center max-w-xl mx-auto lg:mx-20"
       >
         <CarouselContent>
           {product.images.map((image, i) => (
             <CarouselItem key={i}>
               <div className="p-1">
-                <div className="flex aspect-square items-center justify-center p-2 realative">
+                <div className="flex aspect-square items-center justify-center p-2 relative">
                   <Image
                     key={image}
                     src={image}
@@ -56,6 +57,37 @@ const productPage = async ({ searchParams: { url } }: Props) => {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
+      <div className="flex-1 border rounded-md w-full p-5 space-y-5">
+        <h1 className="text-3xl font-bold">{product.title}</h1>
+        <div className="space-x-2">
+          {product.breadcrumbs.map((breadcrumb, i) => (
+            <Badge
+              key={breadcrumb + i}
+              className={breadcrumb}
+              variant="outline"
+            >
+              {breadcrumb}
+            </Badge>
+          ))}
+        </div>
+        <div
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          className="py-5"
+        />
+        {product.rating && (
+          <p className="text-yellow-500 text-sm">
+            {product.rating.rating}★
+            <span className="text-gray-400 ml-2">
+              ({product.rating.count} reviews)
+            </span>
+          </p>
+        )}
+        <p className="text-2xl font-bold mt-2">
+          {product?.currency} {product.price}
+        </p>
+        {/* Add to Cart Button */}
+        <hr />
+      </div>
     </div>
   );
 };
